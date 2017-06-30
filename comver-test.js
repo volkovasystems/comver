8574c9a550ae502ec4f6b37b70aedb3e3aca88ea
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-30 21:15:25 +0800

    Updated test.

--- comver-test.js
+++ comver-test.js
@@ -1,10 +1,15 @@
 
+const assert = require( "assert" );
 const comver = require( "./comver.js" );
+const truly = require( "truly" );
+const filled = require( "filled" );
 
-console.log( comver( "mongod" ).execute( true ) );
+assert.equal( truly( comver( "mongod" ).execute( true ) ), true, "should be true" );
 
-console.log( comver( "node" ).execute( true ) );
+assert.equal( truly( comver( "node" ).execute( true ) ), true, "should be true" );
 
 comver( "npm" ).execute( )( function done( error, result ){
-	console.log( arguments );
+	assert.equal( filled( Array.from( arguments ) ), true, "should be true" );
 } );
+
+console.log( "ok" );
